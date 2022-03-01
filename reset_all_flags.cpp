50e767bcfe13985f2b30ac58336f12a094cad332
Ognjen Stefanovic <os415@cam.ac.uk>
2022-03-01 14:50:16 +0000

    Upload New File

---
+++ reset_all_flags.cpp
@@ -0,0 +1,11 @@
+#include "header.h"
+
+void reset_all_flags(void)
+{
+    // function to reset all flags
+    
+    i_am_going_forward = false;
+    i_am_going_backward = false;
+    i_am_turning_left = false;
+    i_am_turning_right = false;
+}
