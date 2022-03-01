0073c63a63e8a2c33dbb381e5b9b9b7b55622466
Ognjen Stefanovic <os415@cam.ac.uk>
2022-03-01 10:47:07 +0000

    Upload New File

---
+++ test_2_drive_in_a_square.cpp
@@ -0,0 +1,43 @@
+#include "header.h"
+
+void test_drive_in_a_square(uint8_t velocity)
+{
+    // robot goes froward for 5 seconds
+    go_forward(velocity);
+    delay(5000);
+    
+
+    // need to figure out how to make the robot turn by 90 degrees
+    // turn 90 degrees
+    turn_right(100, 100);
+    delay(3000);
+
+    
+
+    go_forward(velocity);
+    delay(5000);
+
+
+    turn_right(100, 100);
+    delay(3000);
+    
+
+    go_forward(velocity);
+    delay(5000);
+
+
+    turn_right(100, 100);
+    delay(3000);
+    
+    
+    go_forward(velocity);
+    delay(5000);
+
+
+    turn_right(100, 100);
+    delay(3000);
+    
+
+    right_wheel_motor->run(RELEASE);
+    left_wheel_motor->run(RELEASE);
+}
