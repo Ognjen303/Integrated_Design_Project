528b22bbe00d47601888bac8109dcc8e9f9d400c
Ognjen Stefanovic <os415@cam.ac.uk>
2022-03-01 15:44:47 +0000

    Replace servo_rotating.cpp

--- servo_rotating.cpp
+++ servo_rotating.cpp
@@ -10,13 +10,11 @@ int pos = 0;    // variable to store the servo position
 
 void servo_rotating(void)
 {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 90; pos += 1)
+  { // goes from 0 degrees to 90 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  
 }
