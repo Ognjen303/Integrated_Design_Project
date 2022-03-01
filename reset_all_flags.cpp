c673a5b91fe9ee84266ba22a943fbd8a8a66f197
Ognjen Stefanovic <os415@cam.ac.uk>
2022-03-01 15:43:15 +0000

    Replace reset_all_flags.cpp

--- reset_all_flags.cpp
+++ reset_all_flags.cpp
@@ -8,4 +8,7 @@ void reset_all_flags(void)
     i_am_going_backward = false;
     i_am_turning_left = false;
     i_am_turning_right = false;
+    i_am_detecting_colour = false;
+    i_am_detecting_red_colour = false;
+    i_am_detecting_blue_colour = false;
 }
