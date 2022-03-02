ee7105f1e920571523d86716a8282dd699d240ec
Q.gao <qg223@cam.ac.uk>
2022-03-02 14:43:00 +0000

    servo code updated

---
+++ main/servo_basic.cpp
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+
+  for (pos = 0; pos <= 110; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  delay(5000);
+  for (pos = 110; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  delay(5000);
+}
