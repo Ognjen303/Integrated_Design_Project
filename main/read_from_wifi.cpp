9763ba1772b90749676866bfed55fea6478048da
Ognjen Stefanovic <os415@cam.ac.uk>
2022-03-03 13:37:05 +0000

    Upload New File

---
+++ main/read_from_wifi.cpp
@@ -0,0 +1,24 @@
+#include "header.h"
+
+String incomingStr;
+
+void read_from_wifi(void) {
+  
+  incomingStr = "";
+  int messageSize = mqttClient.parseMessage();
+  if (messageSize)
+  {
+    if (mqttClient.available()) {
+      incomingStr = mqttClient.readString();
+      //Serial.println(incomingStr);
+    }
+    for (int i = 0; i <= incomingStr.length(); i++) {
+      if (incomingStr.substring(i, i + 1) == ";") {
+        angle = incomingStr.substring(0, i).toFloat();
+        distance = incomingStr.substring(i + 1, incomingStr.length()).toFloat();
+        //Serial.println(angle);
+        //break;
+      }
+    }
+  }
+}
