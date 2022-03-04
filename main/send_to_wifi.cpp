d94c84e7652e3ae3183a716b96c4cbbd60106da8
I.W. Webber <iww22@cam.ac.uk>
2022-03-04 19:59:13 +0000

    Upload New File

---
+++ main/send_to_wifi.cpp
@@ -0,0 +1,36 @@
+#include "header.h"
+
+const long send_interval = 1000;
+unsigned long previousSendMillis = 0;
+
+int send_count = 0;
+
+/*
+void send_to_wifi(void){
+  unsigned long currentSendMillis = millis();
+
+  if (currentSendMillis - previousSendMillis >= send_interval)
+      {
+        // save the last time a message was sent
+        previousSendMillis = currentSendMillis;
+    
+        Serial.print("Sending message to topic: ");
+        Serial.println(topic);
+  
+        //Serial.println(message);      
+        
+        // send message, the Print interface can be used to set the message contents
+        mqttClient.beginMessage(topic);
+        /*
+        mqttClient.print("hello ");
+        mqttClient.print(count);
+        
+
+        mqttClient.print(message);
+        mqttClient.endMessage();
+    
+        Serial.println();
+    
+        count++;
+      }
+}*/
