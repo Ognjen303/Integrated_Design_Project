3530030eaea58cf7f94f48aed6400bdbdba8b73c
I.W. Webber <iww22@cam.ac.uk>
2022-03-04 19:57:51 +0000

    Replace stop_the_robot.cpp

--- main/stop_the_robot.cpp
+++ main/stop_the_robot.cpp
@@ -6,7 +6,10 @@ bool i_stopped = false;
 
 void stop_the_robot(void)
 {
-    if(i_stopped)
+      right_wheel_motor->run(RELEASE);
+      left_wheel_motor->run(RELEASE);
+  
+    /*if(i_stopped)
       return;
 
     else
@@ -17,5 +20,5 @@ void stop_the_robot(void)
       left_wheel_motor->run(RELEASE);
   
       i_stopped = true;
-    }
+    }*/
 }
