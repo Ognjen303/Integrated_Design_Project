1ef1ca4bb0eb0948a9b03c07fadef7be921790d9
I.W. Webber <iww22@cam.ac.uk>
2022-03-08 11:54:51 +0000

    Replace send_to_wifi.cpp

--- main/send_to_wifi.cpp
+++ main/send_to_wifi.cpp
@@ -5,32 +5,35 @@ unsigned long previousSendMillis = 0;
 
 int send_count = 0;
 
-/*
-void send_to_wifi(void){
-  unsigned long currentSendMillis = millis();
+void send_to_wifi(void) {
 
-  if (currentSendMillis - previousSendMillis >= send_interval)
+  mqttClient.beginMessage(topic);
+  mqttClient.print("Sending to python");
+  mqttClient.endMessage();
+  /*unsigned long currentSendMillis = millis();
+
+    if (currentSendMillis - previousSendMillis >= send_interval)
       {
         // save the last time a message was sent
         previousSendMillis = currentSendMillis;
-    
+
         Serial.print("Sending message to topic: ");
         Serial.println(topic);
-  
-        //Serial.println(message);      
-        
+
+        //Serial.println(message);
+
         // send message, the Print interface can be used to set the message contents
         mqttClient.beginMessage(topic);
-        /*
+
         mqttClient.print("hello ");
         mqttClient.print(count);
-        
+
 
         mqttClient.print(message);
         mqttClient.endMessage();
-    
+
         Serial.println();
-    
+
         count++;
-      }
-}*/
+      }*/
+}
