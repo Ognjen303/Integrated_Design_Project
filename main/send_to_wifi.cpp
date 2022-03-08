54f7afb0f86584bee1074e861cec009851e1b751
I.W. Webber <iww22@cam.ac.uk>
2022-03-08 12:19:08 +0000

    Replace send_to_wifi.cpp

--- main/send_to_wifi.cpp
+++ main/send_to_wifi.cpp
@@ -5,10 +5,14 @@ unsigned long previousSendMillis = 0;
 
 int send_count = 0;
 
-void send_to_wifi(void) {
+void send_to_wifi(String send_message) {
+
+  Serial.println(topic);
+  
+  mqttClient.poll();
 
   mqttClient.beginMessage(topic);
-  mqttClient.print("Sending to python");
+  mqttClient.print(send_message);
   mqttClient.endMessage();
   /*unsigned long currentSendMillis = millis();
 
