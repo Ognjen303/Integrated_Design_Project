8dadcc14760de7ac8b37f83f8f27bd1a0519bf51
Kohei Watanabe <kw529@cam.ac.uk>
2022-03-08 12:04:51 +0000

    Create servo stalling code

--- main/servo_rotating.cpp
+++ main/servo_rotating.cpp
@@ -1,20 +1,40 @@
 #include "header.h"
 
-
-
 Servo myservo;
-const uint8_t servoPin = 10;
+const uint8_t servoPin = 3;
+int pos;
+const unsigned long servo_rotate_interval = 15;
+unsigned long servo_timer;
 
-int pos = 0;    // variable to store the servo position
 
+void servo_forward (void){
 
-void servo_rotating(void)
-{
-  for (pos = 0; pos <= 90; pos += 1)
-  { // goes from 0 degrees to 90 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  Serial.println("I am in servo_forward");
   
+  while (pos <= 120){
+      Serial.println("I am inside first if stetement of servo_forward");
+
+    if ( (millis () - servo_timer) > servo_rotate_interval){
+      servo_timer = millis ();
+      pos += 1;
+      myservo.write(pos);
+      Serial.println(pos);
+    }
+    if (servo.read()-120 > 3 ){
+    //servo is stalling
+      i_am_stalling = 1;
+      servo.write(servo.read());
+    }
+  }
+}
+
+void servo_backward (void){
+  while (0 <= pos <= 120){
+    if ( (millis () - servo_timer) > servo_rotate_interval){
+      servo_timer = millis ();
+      pos -= 1;
+      myservo.write(pos);
+      Serial.println(pos);
+    }
+  }
 }
